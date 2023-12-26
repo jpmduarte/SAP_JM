00a0ff372809457015d90f26bd549f800604328d
Diogo Pinheiro <a24016@alunos.ipca.pt>
2023-12-26 20:16:20 +0000

    angular

--- angular-TP_global/src/app/app.component.spec.ts
+++ angular-TP_global/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-TP_global' title`, () => {
+  it(`should have the 'angular-TP_Global' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-TP_global');
+    expect(app.title).toEqual('angular-TP_Global');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-TP_global');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-TP_Global');
   });
 });
--- angular-TP_global/src/app/app.routes.ts
+++ angular-TP_global/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UtenteDashboardComponent } from './utente-dashboard/utente-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'register', component: RegisterComponent},
+    { path: 'utente_dashboard', component : UtenteDashboardComponent},
+    { path: 'admin_dashboard', component : AdminDashboardComponent},
+];
+
