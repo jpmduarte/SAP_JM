52f05d06f2a95f61fad79f4b9662aabe54389392
Diogo Pinheiro <a24016@alunos.ipca.pt>
2023-12-28 18:55:43 +0000

    login component

--- angular-TP_global/src/app/login/login.component.ts
+++ angular-TP_global/src/app/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  
 })
 export class LoginComponent {
 
+  showLoginForm = true;
+
+  toggleForm(event: Event): void {
+    event.preventDefault();
+    this.showLoginForm = !this.showLoginForm;
+  }
 }
