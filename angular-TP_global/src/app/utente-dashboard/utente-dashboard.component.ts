eca0af57454ac0df5cfb6affa590acf35ae07459
Diogo Pinheiro <a24016@alunos.ipca.pt>
2023-12-30 21:36:32 +0000

    utente dashboard

--- angular-TP_global/src/app/utente-dashboard/utente-dashboard.component.ts
+++ angular-TP_global/src/app/utente-dashboard/utente-dashboard.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-utente-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './utente-dashboard.component.html',
-  styleUrl: './utente-dashboard.component.css'
+  styleUrls: ['./utente-dashboard.component.css']
 })
 export class UtenteDashboardComponent {
+  
+  showLogoutPopup = false;
+  showFormPopup = false;
+  showPreviousAssessmentField: boolean = false;
 
+  constructor(private router: Router) {}
+
+  toggleLogoutPopup() {
+    this.showLogoutPopup = !this.showLogoutPopup;
+  }
+
+  toggleFormPopup() {
+    this.showFormPopup = !this.showFormPopup;
+  }
+
+  logout(): void {
+    this.showLogoutPopup = false;
+    this.router.navigate(['/login']);
+  }
+
+  onSubmissionStatusChange(value: string): void {
+    if (value === 'sim') {
+      this.showPreviousAssessmentField = true;
+    } else {
+      this.showPreviousAssessmentField = false;
+    }
+  }
 }
