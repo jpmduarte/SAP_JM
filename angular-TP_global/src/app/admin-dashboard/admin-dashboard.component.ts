38c52e8ac1a02ee526967ef312618bd3deeea78b
Diogo Pinheiro <a24016@alunos.ipca.pt>
2024-01-02 21:50:33 +0000

    updates

--- angular-TP_global/src/app/admin-dashboard/admin-dashboard.component.ts
+++ angular-TP_global/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
