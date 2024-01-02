9b9f293a0954495074d30faed0984b485f40a62a
Diogo Pinheiro <a24016@alunos.ipca.pt>
2024-01-02 21:50:02 +0000

    utente

--- angular-TP_global/src/app/utente-dashboard/utente-dashboard.component.ts
+++ angular-TP_global/src/app/utente-dashboard/utente-dashboard.component.ts
@@ -11,9 +11,11 @@ import { Router } from '@angular/router';
 })
 export class UtenteDashboardComponent {
   
+  show = true;
   showLogoutPopup = false;
   showFormPopup = false;
   showPreviousAssessmentField: boolean = false;
+  files: File[] = [];
 
   constructor(private router: Router) {}
 
@@ -23,6 +25,7 @@ export class UtenteDashboardComponent {
 
   toggleFormPopup() {
     this.showFormPopup = !this.showFormPopup;
+    this.show = !this.show;
   }
 
   logout(): void {
@@ -36,5 +39,45 @@ export class UtenteDashboardComponent {
     } else {
       this.showPreviousAssessmentField = false;
     }
+  }
+
+  onFileChange(event: any) {
+    const selectedFiles = event.target.files;
+    for (let i = 0; i < selectedFiles.length; i++) {
+      this.files.push(selectedFiles[i]);
+    }
+  }
+
+  removeFile(file: File) {
+    const index = this.files.indexOf(file);
+    if (index !== -1) {
+      this.files.splice(index, 1);
+    }
+  }
+
+  openFile(fileToOpen: File) {
+    // Create a new blob object
+    const blob = new Blob([fileToOpen], { type: fileToOpen.type });
+    
+    // Create a link element
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    
+    // Set link's href to point to the Blob URL
+    link.href = url;
+    link.download = fileToOpen.name;
+    
+    // Append link to the body
+    document.body.appendChild(link);
+    
+    // Dispatch click event on the link
+    // This will download the file
+    link.click();
+    
+    // Remove link from body
+    document.body.removeChild(link);
+    
+    // Release the Blob URL
+    window.URL.revokeObjectURL(url);
   }
 }
