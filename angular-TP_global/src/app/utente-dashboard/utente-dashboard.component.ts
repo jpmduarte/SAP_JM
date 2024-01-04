7b941c62afd1b972deb4d962f1a10570ed075a84
Diogo Pinheiro <a24016@alunos.ipca.pt>
2024-01-04 18:25:28 +0000

    updates

--- angular-TP_global/src/app/utente-dashboard/utente-dashboard.component.ts
+++ angular-TP_global/src/app/utente-dashboard/utente-dashboard.component.ts
@@ -56,28 +56,17 @@ export class UtenteDashboardComponent {
   }
 
   openFile(fileToOpen: File) {
-    // Create a new blob object
     const blob = new Blob([fileToOpen], { type: fileToOpen.type });
     
-    // Create a link element
     const url = window.URL.createObjectURL(blob);
     const link = document.createElement('a');
     
-    // Set link's href to point to the Blob URL
     link.href = url;
     link.download = fileToOpen.name;
     
-    // Append link to the body
     document.body.appendChild(link);
-    
-    // Dispatch click event on the link
-    // This will download the file
     link.click();
-    
-    // Remove link from body
     document.body.removeChild(link);
-    
-    // Release the Blob URL
     window.URL.revokeObjectURL(url);
   }
 }
