b7f321d374b31712b3790aa1102bb2c0828396c2
Diogo Pinheiro <a24016@alunos.ipca.pt>
2024-01-10 12:03:16 +0000

    10/01 html, css

--- SAP_JM/src/app/utente-dashboard/utente-dashboard.component.ts
+++ SAP_JM/src/app/utente-dashboard/utente-dashboard.component.ts
@@ -17,18 +17,17 @@ export class UtenteDashboardComponent {
   showSuccessPopup = false;
   files: File[] = [];
 
-  UsfAssociada = {
-    id_utente: '',
-    nomeUSF: '',
-  };
-
-  UserInfo = {
+  primeiroPedido: any = {
+    idUtente: '',
+    idMedico: '',
+    estado: '',
+    descricao: '',
     nomeCompleto: '',
-    numeroUtente: '',
-    dataNascimento: '',
+    dataNascimento:Date,
     nIdentificacao: '',
+    nUtenteSaude: '',
     nif: '',
-    DataDeValidade: '',
+    dataValidade:Date,
     rua: '',
     codigoPostal: '',
     localidade: '',
@@ -36,6 +35,15 @@ export class UtenteDashboardComponent {
     distrito: '',
     telemovel: '',
     email: '',
+    multiuso: false,
+    importacaoVeiculo: false,
+    submissaoReavaliacao: false,
+    dataSubmissaoReavaliacao:Date,
+  };
+
+  UtenteUSF: any = {
+    id_utnete: '',
+    nome_usf: ''
   };
 
   constructor(private router: Router, private http: HttpClient,private activatedRoute: ActivatedRoute,) {}
@@ -51,26 +59,27 @@ export class UtenteDashboardComponent {
       return;
     }
   
-    // Include the email parameter in the URL
+    
     const url = `http://localhost:3002/api/userinfo?email=${email}`;
   
-    // Make the HTTP request with the updated URL
+
     this.http.get(url).subscribe(
       (response: any) => {
         console.log(response);
-        this.UserInfo.nomeCompleto = response.nomeCompleto;
-        this.UserInfo.numeroUtente = response.numeroUtente;
-        this.UserInfo.dataNascimento = response.dataNascimento;
-        this.UserInfo.nIdentificacao = response.nIdentificacao;
-        this.UserInfo.nif = response.nif;
-        this.UserInfo.DataDeValidade = response.DataDeValidade;
-        this.UserInfo.rua = response.rua;
-        this.UserInfo.codigoPostal = response.codigoPostal;
-        this.UserInfo.localidade = response.localidade;
-        this.UserInfo.concelho = response.concelho;
-        this.UserInfo.distrito = response.distrito;
-        this.UserInfo.telemovel = response.telemovel;
-        this.UserInfo.email = response.email;
+        this.primeiroPedido.nomeCompleto = response.nomeCompleto;
+        this.primeiroPedido.numeroUtente = response.numeroUtente;
+        this.primeiroPedido.dataNascimento = response.dataNascimento;
+        this.primeiroPedido.nIdentificacao = response.nIdentificacao;
+        this.primeiroPedido.nif = response.nif;
+        this.primeiroPedido.DataDeValidade = response.DataDeValidade;
+        this.primeiroPedido.rua = response.rua;
+        this.primeiroPedido.codigoPostal = response.codigoPostal;
+        this.primeiroPedido.localidade = response.localidade;
+        this.primeiroPedido.concelho = response.concelho;
+        this.primeiroPedido.distrito = response.distrito;
+        this.primeiroPedido.telemovel = response.telemovel;
+        this.primeiroPedido.email = response.email;
+
       },
       (error) => {
         console.log(error);
@@ -131,6 +140,55 @@ export class UtenteDashboardComponent {
 
   submitForm() {
     this.showSuccessPopup = true;
+    this.http.post('http://localhost:3001/api/submit/pedidoAvaliacao', this.primeiroPedido).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  fetchPedidos() {
+    const email = this.activatedRoute.snapshot.queryParams['email'];
+  
+    this.http.get(`http://localhost:3001/api/pedidos?email=${email}`).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  
+  fetchUtenteUSF() {
+    this.http.get('http://localhost:3002/api/utenteUSF').subscribe(
+      (response) => {
+        console.log(response);
+        this.UtenteUSF = response;
+
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  postUtenteUSF() {
+    const utenteUSFData = {
+      id_utente: this.UtenteUSF.id_utente, 
+      id_USF: this.UtenteUSF.id_USF, 
+    };
+  
+    this.http.post('http://localhost:3001/api/utenteUSF', utenteUSFData).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   closePopup() {
