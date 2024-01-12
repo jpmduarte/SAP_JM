30f8b4881d990f19995af02be3198b129b6c04c9
Diogo Pinheiro <a24016@alunos.ipca.pt>
2024-01-12 00:59:27 +0000

    Admin dashboard

--- SAP_JM/src/app/admin-dashboard/admin-dashboard.component.ts
+++ SAP_JM/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -33,6 +33,17 @@ export class AdminDashboardComponent {
   ScheduleTimes: string[] = ['7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
   users: User[] = [];
   selectedUser: User | undefined;
+  doctorsWithNoSchedule: Employee[] = [];
+  doctorswithSchedules: Employee[] = [];
+  selectedpsaude: any;
+  selectedUpdatepsaude: any;
+  showCreateForm = false;
+  selectedDayOfWeek!: number;
+  selectedMorningStart: string = '';
+  selectedMorningEnd: string = '';
+  selectedAfternoonStart: string = '';
+  selectedAfternoonEnd: string = '';
+  showAlterScheduleform = false;
 
   constructor(private router: Router, private http: HttpClient) {}
 
@@ -58,7 +69,6 @@ export class AdminDashboardComponent {
     );
   }
   
-
   toggleLogoutPopup() {
     this.showLogoutPopup = !this.showLogoutPopup;
   }
@@ -86,7 +96,6 @@ export class AdminDashboardComponent {
     this.showCreateEmployeeForm = false;
   }
 
-
   confirmDelete(user: User) {
     this.selectedUser = user;
   }
@@ -95,18 +104,18 @@ export class AdminDashboardComponent {
     this.selectedUser = undefined;
   }
 
-    deleteUser(user: User): void {
-      const userId = user.id_user;
-      this.http.put(`/api/deactivateusers/${userId}`, {}).subscribe(
-        () => {
-          console.log('User deleted successfully');
-          this.fetchUsers();
-        },
-        (error: any) => {
-          console.error('Error deleting user:', error);
-        }
-      );
-    }
+  deleteUser(user: User): void {
+    const userId = user.id_user;
+    this.http.put(`/api/deactivateusers/${userId}`, {}).subscribe(
+      () => {
+        console.log('User deleted successfully');
+        this.fetchUsers();
+      },
+      (error: any) => {
+        console.error('Error deleting user:', error);
+      }
+    );
+  }
 
   // submitCreateEmployeeForm(employee: any) {
   //     employee.accountType = Number(employee.accountType) + 1;
@@ -134,35 +143,59 @@ export class AdminDashboardComponent {
   //     );
   // }
 
+  createschedule(medico: Employee) {
+    this.selectedpsaude = medico;
+    this.showCreateForm = true;
+  }
+
+  alterschedule(medico: Employee) {
+    this.selectedUpdatepsaude = medico;
+    this.showAlterScheduleform = true;
+  }
+
   updateUser(user: User) {
     this.selectedUserToUpdate = user;
   }
 
-    submitUpdateForm() {
-      if (this.selectedUserToUpdate) {
-        const userId = this.selectedUserToUpdate.id_user;
-        const { nome, nomeperfil, email, password } = this.selectedUserToUpdate;
-
-        const body = {
-          nome: nome,
-          nomeperfil: nomeperfil,
-          email: email,
-          password: password,
-        };
-
-        this.http.put(`/api/updateusers/${userId}`, body).subscribe(
-          () => {
-            console.log('User updated successfully');
-          },
-          (error: any) => {
-            console.error('Error updating user:', error);
-          }
-        );
-      }
+  submitUpdateForm() {
+    if (this.selectedUserToUpdate) {
+      const userId = this.selectedUserToUpdate.id_user;
+      const { nome, nomeperfil, email, password } = this.selectedUserToUpdate;
+      const body = {
+        nome: nome,
+        nomeperfil: nomeperfil,
+        email: email,
+        password: password,
+      };
+      this.http.put(`/api/updateusers/${userId}`, body).subscribe(
+        () => {
+          console.log('User updated successfully');
+        },
+        (error: any) => {
+          console.error('Error updating user:', error);
+        }
+      );
     }
+  }
 
   cancelUpdate() {
     this.selectedUserToUpdate = undefined;
+  }
+
+  cancelCreateSchedule() {
+    // Reset the form values
+    this.selectedDayOfWeek;
+    this.selectedMorningStart = '';
+    this.selectedMorningEnd = '';
+    this.selectedAfternoonEnd = '';
+    this.selectedAfternoonStart = '';
+
+    // Hide the create form
+    this.showCreateForm = false;
+  }
+
+  cancelAlterSchedule() {
+    this.showAlterScheduleform = false;
   }
 
 }
