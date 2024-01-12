83de55a99e6b398bc1cacecf7eb6f988274a12d7
jpmduarte <joaopauloduarte2001@gmail.com>
2024-01-12 00:02:09 +0000

    something is worng

--- SAP_JM/src/app/utente-dashboard/utente-dashboard.component.ts
+++ SAP_JM/src/app/utente-dashboard/utente-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
